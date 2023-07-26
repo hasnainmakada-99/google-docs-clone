4b6dc5abaa53a8ed547654b206f4be0bece6d1b4
Hasnain Makada <hasnainmakada@gmail.com>
2023-07-26 15:34:14 +0530

    [Not-solving]: Some expert needed

--- lib/models/UserModel.dart
+++ lib/models/UserModel.dart
@@ -1,59 +1,117 @@
+// import 'dart:convert';
+
+// // ignore_for_file: public_member_api_docs, sort_constructors_first
+
+// class UserModel {
+//   String name;
+//   String uid;
+//   String email;
+//   String profilePic;
+//   String token;
+
+//   UserModel({
+//     required this.name,
+//     required this.uid,
+//     required this.email,
+//     required this.profilePic,
+//     required this.token,
+//   });
+
+//   Map<String, dynamic> toMap() {
+//     return <String, dynamic>{
+//       'name': name,
+//       'uid': uid,
+//       'email': email,
+//       'profilePic': profilePic,
+//       'token': token,
+//     };
+//   }
+
+//   factory UserModel.fromMap(Map<String, dynamic> map) {
+//     return UserModel(
+//       name: map['name'] as String,
+//       uid: map['_id'] as String,
+//       email: map['email'] as String,
+//       profilePic: map['profilePic'] as String,
+//       token: map['token'] as String,
+//     );
+//   }
+
+//   String toJson() => json.encode(toMap());
+
+//   factory UserModel.fromJson(String source) =>
+//       UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+//   UserModel copyWith({
+//     String? name,
+//     String? uid,
+//     String? email,
+//     String? profilePic,
+//     String? token,
+//   }) {
+//     return UserModel(
+//       name: name ?? this.name,
+//       uid: uid ?? this.uid,
+//       email: email ?? this.email,
+//       profilePic: profilePic ?? this.profilePic,
+//       token: token ?? this.token,
+//     );
+//   }
+// }
+
 import 'dart:convert';
 
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-
 class UserModel {
-  String name;
-  String uid;
-  String email;
-  String profilePic;
-  String token;
-
+  final String email;
+  final String name;
+  final String profilePic;
+  final String uid;
+  final String token;
   UserModel({
-    required this.name,
-    required this.uid,
     required this.email,
+    required this.name,
     required this.profilePic,
+    required this.uid,
     required this.token,
   });
 
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'name': name,
-      'uid': uid,
+    return {
       'email': email,
+      'name': name,
       'profilePic': profilePic,
+      'uid': uid,
       'token': token,
     };
   }
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
-      name: map['name'] as String,
-      uid: map['_id'] as String,
-      email: map['email'] as String,
-      profilePic: map['profilePic'] as String,
-      token: map['token'] as String,
+      email: map['email'] ?? '',
+      name: map['name'] ?? '',
+      profilePic: map['profilePic'] ?? '',
+      uid: map['_id'] ?? '',
+      token: map['token'] ?? '',
     );
   }
 
   String toJson() => json.encode(toMap());
 
   factory UserModel.fromJson(String source) =>
-      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+      UserModel.fromMap(json.decode(source));
 
   UserModel copyWith({
-    String? name,
-    String? uid,
     String? email,
+    String? name,
     String? profilePic,
+    String? uid,
     String? token,
   }) {
     return UserModel(
-      name: name ?? this.name,
-      uid: uid ?? this.uid,
       email: email ?? this.email,
+      name: name ?? this.name,
       profilePic: profilePic ?? this.profilePic,
+      uid: uid ?? this.uid,
       token: token ?? this.token,
     );
   }
--- lib/repository/AuthRepository.dart
+++ lib/repository/AuthRepository.dart
@@ -1,16 +1,15 @@
 import 'dart:convert';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs_clone/constants.dart';
 import 'package:google_docs_clone/models/ErrorModel.dart';
 import 'package:google_docs_clone/models/UserModel.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 final authRepositoryProvider = Provider<AuthRepository>(
   (_) => AuthRepository(
     googleSignIn: GoogleSignIn(),
-    client: http.Client(),
+    client: Client(),
   ),
 );
 
@@ -18,34 +17,32 @@ final userProvider = StateProvider<UserModel?>((ref) => null);
 
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
-  final http.Client _client;
+  final Client _client;
   // we have provided another instance of google sign in inside the authrepository constructor becasue if we inidialize it with the private constructor, then our code might be breakable
-  AuthRepository(
-      {required GoogleSignIn googleSignIn, required http.Client client})
+  AuthRepository({required GoogleSignIn googleSignIn, required Client client})
       : _googleSignIn = googleSignIn,
         _client =
             client; // here we have ensured that the instance which we are passing inside the constructor is assigned to to the private instance _googleSignIn and also of http.client
 
   Future<ErrorModel> googleSignIn() async {
     ErrorModel error = ErrorModel(
-      error: 'Some Unexpected Error Occured',
+      error: 'Some unexpected error occurred.',
       data: null,
     );
+    print(error.error);
     try {
       final user = await _googleSignIn.signIn();
       if (user != null) {
         final userAcc = UserModel(
-          name: user.displayName ?? '',
-          uid: '',
           email: user.email,
+          name: user.displayName ?? '',
           profilePic: user.photoUrl ?? '',
+          uid: '',
           token: '',
         );
-        print(user.email);
-        print(user.photoUrl);
-        print(user.displayName);
+
         var res = await _client.post(
-          Uri.parse('$HOST/api/signup'),
+          Uri.parse('$host/api/signup'),
           body: userAcc.toJson(),
           headers: {
             'Content-Type': 'application/json; charset=UTF-8',
@@ -58,9 +55,9 @@ class AuthRepository {
               uid: jsonDecode(res.body)['user']['_id'],
               token: jsonDecode(res.body)['token'],
             );
-
             error = ErrorModel(error: null, data: newUser);
-          default:
+
+            break;
         }
       }
     } catch (e) {
