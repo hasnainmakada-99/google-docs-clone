b8566d9808ac58e6dee5a00897c9d9992fbd1ecd
Hasnain Makada <hasnainmakada@gmail.com>
2023-07-29 15:06:00 +0530

    [update]: some changes in colors

--- lib/utilities/colors.dart
+++ lib/utilities/colors.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const kWhiteColor = Colors.white;
 const kBlackColor = Colors.black;
-const kRedColor = Colors.red;
+const kRedColor = Color.fromARGB(255, 255, 17, 0);
